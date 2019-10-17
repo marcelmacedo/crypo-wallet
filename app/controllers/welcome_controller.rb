536f8e4635587be215fa4c36bebea0347508b215
Marcel Macedo <marcel.handock@gmail.com>
2019-10-17 18:22:09 -0400

    Aplicando cookies e sessions na aplicação

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,10 @@
 class WelcomeController < ApplicationController
   def index
+    #Armazena cookies no navegador
+    cookies[:curso] = "Curso ruby on rails Marcel Macedo - cookies"
+    #Sessao no navegador
+    session[:curso] = "Curso ruby on rails Marcel Macedo - Session"
+
     # #nome, @curso é variavel de instancia/sessao.
     @meu_nome = params[:nome]
     @curso = params[:curso]
