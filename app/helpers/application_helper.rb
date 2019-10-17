2ae71f203b7353560c7cfe561e1815bc9eaa3a36
Marcel Macedo <marcel.handock@gmail.com>
2019-10-17 16:22:53 -0400

    Adicionando I18n internacionalização do sistema

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,9 @@
 module ApplicationHelper
+    def locale
+        I18n.locale == :en ? "EUA" : "BRASIL"
+    end
+    
     def data_br(data_us)
         data_us.strftime("%d/%m/%Y")
     end
-    
 end
